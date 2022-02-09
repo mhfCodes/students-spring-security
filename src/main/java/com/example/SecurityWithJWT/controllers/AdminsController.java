8caef514958f363cff41176ba8a014202048febd
mhfCodes <mohammadhossein.farrokhi.78@gmail.com>
2022-02-10 02:34:46 +0330

    added homepage and permited all requests to it

--- src/main/java/com/example/SecurityWithJWT/controllers/AdminsController.java
+++ src/main/java/com/example/SecurityWithJWT/controllers/AdminsController.java
@@ -1,5 +1,12 @@
 package com.example.SecurityWithJWT.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/management/api/students")
 public class AdminsController {
 
+	
+	
 }
