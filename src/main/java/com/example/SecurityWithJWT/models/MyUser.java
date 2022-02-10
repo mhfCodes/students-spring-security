bdaefcf8f2e0c7a923e288a74a4c2e04604314c0
mhfCodes <mohammadhossein.farrokhi.78@gmail.com>
2022-02-10 16:40:30 +0330

    added user and roles model

---
+++ src/main/java/com/example/SecurityWithJWT/models/MyUser.java
@@ -0,0 +1,71 @@
+package com.example.SecurityWithJWT.models;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class MyUser {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(nullable = false, unique = true)
+	private String username;
+	
+	@Column(length = 250)
+	private String password;
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	private Collection<Role> roles = new ArrayList<>();
+	
+	public MyUser() {
+	}
+	
+	public MyUser(String username, String password, Collection<Role> roles) {
+		this.username = username;
+		this.password = password;
+		this.roles = roles;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Collection<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Collection<Role> roles) {
+		this.roles = roles;
+	}
+	
+}
---
+++ src/main/java/com/example/SecurityWithJWT/models/Role.java
@@ -0,0 +1,43 @@
+package com.example.SecurityWithJWT.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Role {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(nullable = false, unique = true)
+	private String roleName;
+	
+	public Role() {
+	}
+
+	public Role(String roleName) {
+		super();
+		this.roleName = roleName;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+	
+}
